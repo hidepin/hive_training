e94112828645082359941609a79b003b9a35bbc4
Hidetoshi IMAI <imai@func.org>
2015-01-05 18:16:57 +0900

    update hive training.

--- hive_training.sql
+++ hive_training.sql
@@ -2,5 +2,17 @@ DROP DATABASE IF EXISTS financials CASCADE;
 
 CREATE DATABASE IF NOT EXISTS financials COMMENT 'Holds all financial tables';
 
-use financials
+use financials;
+
+CREATE TABLE IF NOT EXISTS employees (
+  name STRING COMMENT 'Employee name',
+  salary FLOAT COMMENT 'Employee salary',
+  subordinates ARRAY<STRING> COMMENT 'Names of subordinates',
+  deductions MAP<STRING, FLOAT>
+             COMMENT 'Keys are deductions names, values are percentages',
+  address STRUCT<street:STRING, city:STRING, state:STRING, zip:INT>
+  COMMENT 'Home address'
+  )
+COMMENT 'Description of the table'
+;
 
